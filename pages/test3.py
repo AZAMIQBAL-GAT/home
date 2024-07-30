b41b66ca6a8286f909042f0f3c4d6e1ee08db973
AZAMIQBAL-GAT <136085346+AZAMIQBAL-GAT@users.noreply.github.com>
2024-07-30 13:13:37 +0500

    Update test3.py

--- pages/test3.py
+++ pages/test3.py
@@ -1,4 +1,4 @@
-streamlit==1.34.0
+import streamlit as st
 from datetime import date, timedelta
 import requests
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
